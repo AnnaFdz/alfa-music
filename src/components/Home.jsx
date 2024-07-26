cda4a6a90441b481ee48f43b5829393f20a5532e
Alfredo <alfredomoreno@servtop.com.ar>
2024-07-26 12:59:01 -0300

    Se creó: SideBar.jsx, SideBarChoice.jsx, sidebar.css y se agrego otra imagen. Se modificó Home: se le agrego index.css, se acomodo un poco el html, se agrego el componente SideBar, se generó estados y funciones para manejar el SideBar. Hay que compaginar con los demas contenidos, se sugiere crear un componente para lo demas. El index.css se modificó para controlar los espacios a usar.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,54 +1,67 @@
-//import "../index.css";
+import "../index.css";
 import Card from "./Card"
 import { Paginacion } from "./Paginacion";
+import SideBar from "./SideBar";
 import Tabs from "./Tabs";
+import { useState } from "react";
 
 
 export default function Home() {
-  
+  const [showPlaylistForm, setShowPlaylistForm] = useState(false);
+  const [isModifyingPlaylist, setIsModifyingPlaylist] = useState(false);
+  const [playlistUpdated, setPlaylistUpdated] = useState(false);
+
+  const handlePlaylistCreate = (showForm) => {
+    setShowPlaylistForm(showForm);
+  };
+
+  const handleModifyPlaylist = () => {
+    setIsModifyingPlaylist(true);
+}
 
   return (
     <>
-    <Paginacion/>
-    <div>
-    <Tabs/>
-    </div>
-    <div className="container">
-         
-            
-       
+    <div className="body">
+      <SideBar
+        onPlaylistCreate={handlePlaylistCreate}
+        onModifyingPlaylist={handleModifyPlaylist}
+        setPlaylistUpdated={setPlaylistUpdated}  
+      />
+      <Paginacion/>
+      <div>
+        <Tabs/>
+      </div>
+      <div className="container">
           <div className='box has-background-danger-60'>
-          <div className="columns">
+            <div className="columns">
+              <div className="column">
+                <div className='box'>
+                  <Card/>
+                </div>
+              </div>
+        {/*---------*/}
             <div className="column">
               <div className='box'>
-              <Card/>
-          
+                <Card/>
+              </div>
+            </div>
+        {/*---------*/}
+            <div className="column">
+              <div className='box'>
+                <Card/>
+              </div>
+            </div>
+        {/*---------*/}
+            <div className="column">
+              <div className='box'>
+                <Card/>
+              </div>
+            </div>
           </div>
-          
-        </div>
-        {/*---------*/}
-        <div className="column">
-        <div className='box'>
-        <Card/>
-
-        </div>
-        </div>
-        {/*---------*/}
-        <div className="column">
-        <div className='box'>
-        <Card/>
-        </div>
-        </div>
-        {/*---------*/}
-        <div className="column">
-        <div className='box'>
-        <Card/>
-        </div>
         </div>
       </div>
-      </div>
-      </div>
-      </>
+    </div>
+    </>
     
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,76 @@
+import logo from "./imgs/Logointro.jpeg";
+import SideBarChoice from "./SideBarChoice";
+import useFetch from "../hooks/useFetch";
+import { useEffect, useState } from "react";
+import "../styles/sidebar.css"
+function SideBar({onPlaylistCreate, onModifyingPlaylist, playlistUpdated, setPlaylistUpdated}) {
+    const [{data, isError, isLoading}, doFetch] = useFetch('https://sandbox.academiadevelopers.com/harmonyhub/playlists/',
+        {}
+    );
+    const [isPlaylistExtended, setIsPlaylistExtended] = useState(false);
+
+    const handlePlaylistCreate = () => {
+        onPlaylistCreate(true);
+    }
+
+    useEffect(() => {
+            doFetch();   
+    },[]);
+
+    useEffect(() => {
+        if (playlistUpdated) {
+            doFetch();
+            setPlaylistUpdated(false);
+        }
+    }, [playlistUpdated, doFetch, setPlaylistUpdated])
+
+    const handlePlaylistToggle = () => {
+        setIsPlaylistExtended(!isPlaylistExtended);
+    };
+
+    if (isLoading) return <h1>Cargando...</h1>
+    if (isError) return <h1>Error al traer las playlists</h1>
+    if (!data) return <h1>No hay canciones Disponibles</h1>
+    
+    return (
+        <>
+            <div className="container">
+                <img src={logo} alt="LOGO"/>
+                <SideBarChoice title={"Inicio"}>
+                    <i className="fa-solid fa-house"></i>
+                </SideBarChoice>
+                <SideBarChoice title={"Playlists"} onClick={handlePlaylistToggle}>
+                    <i className="fa-solid fa-music"></i>
+                </SideBarChoice>
+                {isPlaylistExtended && (
+                    <>
+                        <div className="submenu">
+                            <SideBarChoice title={"Crear Playlist"} onClick={handlePlaylistCreate}>
+                                <i className="fa-solid fa-plus"></i>
+                            </SideBarChoice>
+                        </div>
+                        <div className="submenu">
+                            <SideBarChoice title={"Modificar Playlist"} onClick={onModifyingPlaylist}>
+                                <i className="fa-regular fa-pen-to-square"></i>
+                            </SideBarChoice>
+                        </div>
+                        <div className="submenu">
+                            <SideBarChoice title={"Eliminar Playlist"} onClick={onModifyingPlaylist}>
+                                <i className="fa-solid fa-eraser"></i>
+                            </SideBarChoice>
+                        </div>
+                    </>
+                    
+                )}
+
+                <div className="playlist">Playlist</div>
+                <hr />
+                {data && data.map((song) => (
+                    <SideBarChoice key={song.id} title={song.name}/>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default SideBar;
