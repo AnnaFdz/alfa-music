dbeea90bcb8f2168a8a7b70493d4400fcc525b59
Alfredo <alfredomoreno@servtop.com.ar>
2024-08-04 21:11:03 -0300

    Se puede eliminar una cancion agregada a una playlist (si sos el dueño). estilos mejorados

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -4,13 +4,17 @@ import useFetch from "../hooks/useFetch";
 import "../styles/customPlaylist.css";
 import SideBar from "./SideBar";
 import Card from "./Card";
+import { useAuth } from "../contexts/AuthContext";
+import SelectedSong from "./SelectedSong"; // Import the component
 
 function Playlist() {
+    const { token } = useAuth("state");
     const location = useLocation();
     const { playlistID } = location.state;
     const [songs, setSongs] = useState([]);
+    const [entries, setEntries] = useState([]);
     const navigate = useNavigate();
-    
+
     const [{ data: playlistData, isError: playlistError, isLoading: playlistLoading }, doFetchPlaylist] = useFetch(`https://sandbox.academiadevelopers.com/harmonyhub/playlists/${playlistID}/`, {});
 
     useEffect(() => {
@@ -19,31 +23,77 @@ function Playlist() {
         }
     }, []);
 
-     useEffect(() => {
-         if (playlistData) {
-             const fetchSongs = async () => {
-                 try {
-                     const songPromises = playlistData.entries.map(songId => 
-                         fetch(`https://sandbox.academiadevelopers.com/harmonyhub/songs/${songId}/`)
-                             .then(response => response.json())
-                     );
-                     const songData = await Promise.all(songPromises);
-                     setSongs(songData);
-                 } catch (error) {
-                     console.error("Error fetching songs:", error);
-                 }
-             };
-             fetchSongs();
-         }
-     }, [playlistData]);
+    useEffect(() => {
+        if (playlistData) {
+            const fetchSongs = async () => {
+                try {
+                    const songPromises = playlistData.entries.map(songId => 
+                        fetch(`https://sandbox.academiadevelopers.com/harmonyhub/songs/${songId}/`)
+                            .then(response => response.json())
+                    );
+                    const songData = await Promise.all(songPromises);
+                    setSongs(songData);
+                    
+                    // Fetch playlist entries
+                    const entryResponse = await fetch(`https://sandbox.academiadevelopers.com/harmonyhub/playlist-entries/?playlist=${playlistID}`);
+                    const entryData = await entryResponse.json();
+                    setEntries(entryData.results);
+                } catch (error) {
+                    console.error("Error fetching data:", error);
+                }
+            };
+            fetchSongs();
+        }
+    }, [playlistData]);
+
+    const fetchEntryID = async (playlistID, songID) => {
+        try {
+            const response = await fetch(`https://sandbox.academiadevelopers.com/harmonyhub/playlist-entries/?playlist=${playlistID}`);
+            if (!response.ok) throw new Error('Error fetching playlist entries');
+            const data = await response.json();
+            const entry = data.results.find(entry => entry.song === songID);
+            if (!entry) throw new Error('Entry not found');
+            return entry.id;
+        } catch (error) {
+            console.error('Error fetching entry ID:', error);
+            throw error;
+        }
+    };
 
     const handleAddSong = () => {
         navigate("/addSong", { state: { playlistID }});
     };
 
-    const handleRemoveSong = async (songId) => {
-        // Lógica para eliminar la canción de la playlist
+    const handleRemoveSong = async (songID) => {
+        if (!playlistID) {
+            console.error('No se encontró PlaylistID.');
+            return;
+        }
+        try {
+            const entryID = await fetchEntryID(playlistID, songID);
+            const response = await fetch(`https://sandbox.academiadevelopers.com/harmonyhub/playlist-entries/${entryID}/`, 
+                {
+                    method: "DELETE",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Token ${token}`,
+                    },
+                }
+            );
+            
+            if (!response.ok) {
+                const errorText = await response.text();
+                console.error('Error response:', errorText);
+                throw new Error(`Error: ${response.status} ${response.statusText}`);
+            }
+            
+            setEntries(entries.filter(entry => entry.id !== entryID));
+            setSongs(songs.filter(song => song.id !== songID));
+        } catch (error) {
+            console.error('Error al eliminar entry', error);
+        }
     };
+    
 
     if (playlistLoading) return <h1>Cargando...</h1>;
     if (playlistError) return <h1>Error al traer la playlist</h1>;
@@ -61,12 +111,12 @@ function Playlist() {
                     {songs.length > 0 ? (
                         songs.map((song) => (
                             <div className="column is-one-quarter" key={song.id}>
-                                <span className="remove" onClick={handleRemoveSong}>X</span>
+                                <span className="remove" onClick={() => handleRemoveSong(song.id)}>X</span>
                                 <Card song={song}/>
                             </div>
                         ))
                     ) : (
-                        <p>No hay canciones disponibles.</p>
+                        <p className="parrafo">No hay canciones disponibles.</p>
                     )}
                 </div>
             </div>
